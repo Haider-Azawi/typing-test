d143ef0df7044263440c80b6a63fe1f52a399a06
Haider-Azawi <haider.azawi@outlook.com>
2017-03-13 12:24:11 -0700

    User can type a letter and get feedback.

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ for (var i = 0; i < text.length; i++) {
 
 var $first = document.querySelector('span')
 $first.classList.add('current')
+
+document.addEventListener('keypress', function(event) {
+  var $current = document.querySelector('.current')
+  var $currentCharacter = $current.textContent
+  if (event.key === $currentCharacter) {
+    $current.classList.remove('current')
+    $current.nextSibling.classList.add('current')
+  }
+  else {
+    $current.classList.add('wrong')
+  }
+})
+var $wrong = document.querySelectorAll('.wrong')
+var $result = document.createElement('p')
+$result.textContent = 'You have made ' + $wrong.length + ' mistakes!'
+document.body.appendChild($result)
